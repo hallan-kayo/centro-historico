df6a9d63b5c603d7af1b46588ff466dfc90e706c
hallan kayo <hallankayo20@gmail.com>
2020-12-02 14:29:11 -0300

    index effects finished

--- js/app.js
+++ js/app.js
@@ -1,41 +1,17 @@
-// Debounce do Lodash
-debounce = function(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-
-(function(){
-	var $target = $('.anime'),
-			animationClass = 'anime-start',
-			offset = $(window).height() * 3/4;
-
-	function animeScroll() {
-		var documentTop = $(document).scrollTop();
-
-		$target.each(function(){
-			var itemTop = $(this).offset().top;
-			if (documentTop > itemTop - offset) {
-				$(this).addClass(animationClass);
-			} else {
-				$(this).removeClass(animationClass);
-			}
-		});
-	}
-
-	animeScroll();
-
-	$(document).scroll(debounce(function(){
-		animeScroll();
-	}, 200));
-})();
+const target = document.querySelectorAll('[data-anime]')
+const animateClass = 'animate'
+function animeScroll() {
+	const windowTop = window.pageYOffset + (window.innerHeight * 3.5 / 4);
+	target.forEach(function (element) {
+		if ((windowTop) > element.offsetTop) {
+			element.classList.add(animateClass)
+			
+		} else {
+			element.classList.remove(animateClass)
+		}
+	})
+}
+animeScroll()
+window.addEventListener('scroll', function () {
+	animeScroll()
+})
