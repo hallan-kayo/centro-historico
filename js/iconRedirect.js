6fb7e48c2da0d277b8fe7dabcb7e94603f0103b6
hallan-kayo <hallankayo20@gmail.com>
2022-04-01 22:53:41 -0300

    alterações finais

--- js/iconRedirect.js
+++ js/iconRedirect.js
@@ -1,15 +1,3 @@
 function redirectToVisits(){
-    window.location.href = "./agendamentos.html"
-}
-
-function redirectToCollection(){
-    window.location.href = "./acervo.html"
-}
-
-function redirectToGallery(){
-    window.location.href = "./galeriadeimagens.html"
-}
-
-function redirectToHistory(){
-    window.location.href = "./relatos.html"
+    window.open("https://forms.gle/muSBkvfeBtbLWYEp8")
 }
